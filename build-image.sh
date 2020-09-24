673e888244a0c06c19d3716bab31d947151961c8
achraf <achraf.pica@gmail.com>
2020-09-24 21:22:47 +0200

    add hook pipeline

--- build-image.sh
+++ build-image.sh
@@ -6,3 +6,7 @@ JAR_FILE=$(ls build/libs/ | grep "^finance-custom")
 
 docker build . --build-arg jar=build/libs/$JAR_FILE -t ezzefiohez/finance-custom
 docker push ezzefiohez/finance-custom
+
+echo " ######## BUILD CUSTOM DONE ######## "
+
+curl  -X POST http://94.239.109.172:9000/api/webhooks/cc022332-53f4-4d66-be95-df17ae514da0
